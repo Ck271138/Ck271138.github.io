2e794a41d2b7bf20e04437c01577c9d91c268c55
Ck271138 <78458013+Ck271138@users.noreply.github.com>
2023-02-11 11:39:20 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,24 @@
- #!/bin/sh
+#!/bin/bash
 
- # If a command fails then the deploy stops
- set -e
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
- printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+# Genterate file statis
+hugo # if using a theme, replace by `hugo -t <yourtheme>`
 
- # Build the project.
- hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+# pindah ke direktoru publik
+cd public
+# tambahkan perubahan ke Git
+git add -A
 
- # Go To Public folder
- cd public
+# Buat sebuah commit baru
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
 
- # Add changes to git.
- git add .
+# Push atau puload ke Github
+git push origin master
 
- # Commit changes.
- msg="rebuilding site $(date)"
- if [ -n "$*" ]; then
- 	msg="$*"
- fi
- git commit -m "$msg"
-
- # Push source and build repos.
- git push origin master
+# Balik ke direktori sebelumnya
+cd ..
